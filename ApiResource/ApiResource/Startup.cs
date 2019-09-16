7f2f9a66c46e03a7fa32c2ca747eb4cd6c889c29
- <33056275+IMAXZ@users.noreply.github.com>
2019-09-16 22:15:23 +0800

    测试提交

---
+++ ApiResource/ApiResource/Startup.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace ApiResource
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvcCore()
+             .AddAuthorization()
+             .AddJsonFormatters();
+
+            services.AddAuthentication("Bearer")
+                .AddJwtBearer("Bearer", options =>
+                {
+                    options.Authority = "http://localhost:5000"; //IdentityServer 地址
+                    options.RequireHttpsMetadata = false;
+
+                    options.Audience = "api1"; // 与IdentityServer中配置的名称相同
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseAuthentication();
+
+            app.UseMvc();
+        }
+    }
+}
---
+++ ConsoleClient/ConsoleClient/Program.cs
@@ -0,0 +1,71 @@
+﻿using IdentityModel;
+using IdentityModel.Client;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    var client = new HttpClient();
+        //    Task.Run(async () =>
+        //    {
+        //        var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
+        //        if (disco.IsError)
+        //        {
+        //            Console.WriteLine(disco.Error);
+        //            return;
+        //        }
+        //    }).GetAwaiter().GetResult();
+        //    Console.ReadKey();
+        //}
+
+        static async Task Main(string[] args)
+        {
+            // discovery endpoint
+            var client = new HttpClient();
+            var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
+
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            // request accsess token 
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+                ClientId = "console client",
+                ClientSecret = "511536EF-F270-4058-80CA-1C89C192F69A",
+                Scope = "api1"
+            });
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            // call Identity Resource API
+            var apiClient = new HttpClient();
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+            var response = await apiClient.GetAsync("http://localhost:5001/Identity"); // api1中的api地址
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(JsonConvert.SerializeObject(response) );
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
